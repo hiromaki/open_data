0d57a04857c2a746ecbe18aac3eda383cf40c062
hiromaki <h.makikawa@gmail.com>
2015-11-19 00:08:04 +0900

    mac 3

--- app/controllers/csvdata_controller.rb
+++ app/controllers/csvdata_controller.rb
@@ -53,23 +53,27 @@ class CsvdataController < ApplicationController
 
     # end
 
+    url_base = "http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/"
+
     urls = Array.new
     # 防災関連施設ポイントデータ（災害時避難所・一時避難場所）
-    urls.push("http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_hinanbasyo.csv")
+    urls.push(url_base + "mapnavoskdat_hinanbasyo.csv")
     # 防災関連施設ポイントデータ（災害時用へリポート）
-    urls.push("http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_heliport.csv")
+    urls.push(url_base + "mapnavoskdat_heliport.csv")
     # 防災関連施設ポイントデータ（防火水槽など）
-    urls.push("http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_boukasuisou.csv")
+    urls.push(url_base + "mapnavoskdat_boukasuisou.csv")
     # 防災関連施設ポイントデータ（防災スピーカー）
-    urls.push("http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_bousaisp.csv")
+    urls.push(url_base + "mapnavoskdat_bousaisp.csv")
     # 防災関連施設ポイントデータ（津波避難ビル）
-    urls.push("http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_hinanbiru.csv")
+    urls.push(url_base + "mapnavoskdat_hinanbiru.csv")
 
     for url in urls do
 
-        csv = open(url)
+        # csv = open(url, "rb:CP932:UTF-8").read.encode("utf-8", :invalid => :replace, :undef => :replace)
+        csv = open(url, "r:binary").read.encode("CP932", "UTF-8", invalid: :replace, undef: :replace)
+        File.open('/tmp/file_name', 'w') { |file| file.write(csv) }
 
-        @csv_obj = CSV.read(csv, encoding: "CP932:UTF-8", headers: :first_row)
+        @csv_obj = CSV.read('/tmp/file_name', encoding: "CP932:UTF-8", headers: :first_row)
 
         @csv_obj.each do |row|
           open_data = OpenDatum.find_by(shisetsu_id: row[19])
