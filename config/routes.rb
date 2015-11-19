64bde9febf0393cd34895d35851f64ae0e541424
hiromaki <h.makikawa@gmail.com>
2015-11-20 00:11:36 +0900

    mac 20151119-2

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # root 'csvdata#insert'
   root 'csvdata#read'
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  post '/' => 'csvdata#read'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
