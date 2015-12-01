2525101ba963b207c86bcde37940d71068bd8733
hiroki_makikawa <h.makikawa@gmail.com>
2015-12-01 09:39:45 +0900

    20151201

--- lib/tasks/facility_data_insert.rb
+++ lib/tasks/facility_data_insert.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 class Tasks::FacilityDataInsert
   def self.execute
 
-    # bundle exec rails runner Tasks::FacilityDataInsert.execute 
+    # bundle exec rails runner Tasks::FacilityDataInsert.execute
 
     # 施設情報ポイントデータ（全体）
     url = "http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_shisetsuall.csv"
@@ -62,78 +62,78 @@ class Tasks::FacilityDataInsert
 
     for url in urls do
 
-        if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
-            tmp_file = 'tmp\file_name'
-        else
-            tmp_file = "/tmp/file_name"
-        end
+      if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
+          tmp_file = 'tmp\file_name'
+      else
+          tmp_file = "/tmp/file_name"
+      end
 
-        # csv = open(url, "rb:CP932:UTF-8").read.encode("utf-8", :invalid => :replace, :undef => :replace)
-        csv = open(url, "r:binary").read.encode("CP932", "UTF-8", invalid: :replace, undef: :replace)
-        File.open(tmp_file, 'wb') { |file| file.write(csv) }
+      # csv = open(url, "rb:CP932:UTF-8").read.encode("utf-8", :invalid => :replace, :undef => :replace)
+      csv = open(url, "r:binary").read.encode("CP932", "UTF-8", invalid: :replace, undef: :replace)
+      File.open(tmp_file, 'wb') { |file| file.write(csv) }
 
-        # @csv_obj = CSV.read('/tmp/file_name', encoding: "CP932:UTF-8", headers: :first_row)
-        @csv_obj = CSV.read(tmp_file, encoding: "CP932:UTF-8", headers: :first_row)
+      # @csv_obj = CSV.read('/tmp/file_name', encoding: "CP932:UTF-8", headers: :first_row)
+      @csv_obj = CSV.read(tmp_file, encoding: "CP932:UTF-8", headers: :first_row)
 
-        @csv_obj.each do |row|
+      @csv_obj.each do |row|
 
-          facility = Facility.find_by(shisetsu_id: row[8])
+        facility = Facility.find_by(shisetsu_id: row[8])
 
-          # データがなかった場合
-          if facility.nil?
+        # データがなかった場合
+        if facility.nil?
 
-            facility = Facility.new
+          facility = Facility.new
 
-            if url == url_base + "mapnavoskdat_hinanbasyo.csv"
-                facility.x = row[0]
-                facility.y = row[1]
-                facility.shisetsu_name = row[2]
-                facility.syozaichi = row[3]
-                facility.chiku_name = row[4]
-                facility.tel = row[5]
-                facility.syo_bunrui = row[6]
-                facility.category = row[7]
-                facility.shisetsu_id = row[8]
-            elsif url == url_base + "mapnavoskdat_heliport.csv"
-                facility.x = row[0]
-                facility.y = row[1]
-                facility.shisetsu_name = row[2]
-                facility.syozaichi = row[3]
-                facility.category = row[4]
-                facility.shisetsu_id = row[5]
-            elsif url == url_base + "mapnavoskdat_boukasuisou.csv"
-                facility.x = row[0]
-                facility.y = row[1]
-                facility.shisetsu_name = row[2]
-                facility.syozaichi = row[3]
-                facility.chiku_name = row[4]
-                facility.syo_bunrui = row[6]
-                facility.category = row[7]
-                facility.shisetsu_id = row[8]
-            elsif url == url_base + "mapnavoskdat_bousaisp.csv"
-                facility.x = row[0]
-                facility.y = row[1]
-                facility.shisetsu_name = row[2]
-                facility.syozaichi = row[3]
-                facility.syosai_info = row[4]
-                facility.category = row[5]
-                facility.shisetsu_id = row[6]
-            elsif url == url_base + "mapnavoskdat_hinanbiru.csv"
-                facility.x = row[0]
-                facility.y = row[1]
-                facility.shisetsu_name = row[2]
-                facility.syozaichi = row[3]
-                facility.tel = row[4]
-                facility.syosai_info = row[5]
-                facility.category = row[6]
-                facility.shisetsu_id = row[7]
-            end
-                
-            facility.save
-
+          if url == url_base + "mapnavoskdat_hinanbasyo.csv"
+            facility.x = row[0]
+            facility.y = row[1]
+            facility.shisetsu_name = row[2]
+            facility.syozaichi = row[3]
+            facility.chiku_name = row[4]
+            facility.tel = row[5]
+            facility.syo_bunrui = row[6]
+            facility.category = row[7]
+            facility.shisetsu_id = row[8]
+          elsif url == url_base + "mapnavoskdat_heliport.csv"
+            facility.x = row[0]
+            facility.y = row[1]
+            facility.shisetsu_name = row[2]
+            facility.syozaichi = row[3]
+            facility.category = row[4]
+            facility.shisetsu_id = row[5]
+          elsif url == url_base + "mapnavoskdat_boukasuisou.csv"
+            facility.x = row[0]
+            facility.y = row[1]
+            facility.shisetsu_name = row[2]
+            facility.syozaichi = row[3]
+            facility.chiku_name = row[4]
+            facility.syo_bunrui = row[6]
+            facility.category = row[7]
+            facility.shisetsu_id = row[8]
+          elsif url == url_base + "mapnavoskdat_bousaisp.csv"
+            facility.x = row[0]
+            facility.y = row[1]
+            facility.shisetsu_name = row[2]
+            facility.syozaichi = row[3]
+            facility.syosai_info = row[4]
+            facility.category = row[5]
+            facility.shisetsu_id = row[6]
+          elsif url == url_base + "mapnavoskdat_hinanbiru.csv"
+            facility.x = row[0]
+            facility.y = row[1]
+            facility.shisetsu_name = row[2]
+            facility.syozaichi = row[3]
+            facility.tel = row[4]
+            facility.syosai_info = row[5]
+            facility.category = row[6]
+            facility.shisetsu_id = row[7]
           end
 
+          facility.save
+
         end
+
+      end
 
     end
 
