359feee5bead8b19517937cffa4b1922b80f4bc6
hiroki_makikawa <h.makikawa@gmail.com>
2015-12-03 16:39:53 +0900

    20151203

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'facility#index'
   # Example of regular route:
   post '/' => 'facility#search'
+  # get '/:page' => 'facility#search'
   get '/facility/:id' => "facility#show"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
