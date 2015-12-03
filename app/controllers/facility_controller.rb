4b83a1799a2cb5befae4817a9980a727550581b3
hiroki_makikawa <h.makikawa@gmail.com>
2015-12-03 15:30:44 +0900

    20151203

--- app/controllers/facility_controller.rb
+++ app/controllers/facility_controller.rb
@@ -2,36 +2,13 @@ class FacilityController < ApplicationController
 
   def index
 
-    params[:category].nil? ? init = true : init = false
+    chiku_array = Array.new{ Array.new(2)}
 
-    @chiku_array = Array.new{ Array.new(2)}
-    chikus = Chiku.all
+    @facilities = Kaminari.paginate_array(Facility.order("category, chiku_name").all).page(params[:page]).per(10)
 
-    if init
-      @facilities = Kaminari.paginate_array(Facility.order("category, chiku_name").all).page(params[:page]).per(10)
-
-      @chiku_all_check = true
-      chikus.each do |chiku|
-        @chiku_array.push([chiku.name, true])
-      end
-
-    else
-      if params[:category] == ""
-        @facilities = Kaminari.paginate_array(Facility.order("category, chiku_name").where("shisetsu_name like '%" + params[:text][:shisetsu_name] + "%'").where(chiku_name: params[:check][:chikus])).page(params[:page]).per(10)
-      else
-        @facilities = Kaminari.paginate_array(Facility.order("category, chiku_name").where("shisetsu_name like '%" + params[:text][:shisetsu_name] + "%'").where("category like '" + params[:category] + "%'").where(chiku_name: params[:check][:chikus])).page(params[:page]).per(10)
-      end
-      @shisetsu_name_value = params[:text][:shisetsu_name]
-      @category_selected = params[:category]
-
-      @chiku_all_check = true
-      chikus.each do |chiku|
-        @chiku_array.push([chiku.name, chiku_checkbox(params[:check][:chikus], chiku.name)])
-        unless chiku_checkbox(params[:check][:chikus], chiku.name)
-          @chiku_all_check = false
-        end
-      end
-
+    chiku_all_check = true
+    Chiku.all.each do |chiku|
+      chiku_array.push([chiku.name, true])
     end
 
     if @facilities.blank?
@@ -40,12 +17,64 @@ class FacilityController < ApplicationController
 
     @gmap_hash = gmap_hash(@facilities)
 
+    search_param = Hash.new
+    search_param[:chiku_array] = chiku_array
+    search_param[:chiku_all_check] = chiku_all_check
+    search_param[:category_selected] = ""
+    search_param[:shisetsu_name] = ""
+    @search_param = search_param
+    session["search_param"] = @search_param
+
+  end
+
+  def search
+
+    chiku_array = Array.new{ Array.new(2)}
+
+    if params[:category] == ""
+      @facilities = Kaminari.paginate_array(Facility.order("category, chiku_name").where("shisetsu_name like '%" + params[:text][:shisetsu_name] + "%'").where(chiku_name: params[:check][:chikus])).page(params[:page]).per(10)
+    else
+      @facilities = Kaminari.paginate_array(Facility.order("category, chiku_name").where("shisetsu_name like '%" + params[:text][:shisetsu_name] + "%'").where("category like '" + params[:category] + "%'").where(chiku_name: params[:check][:chikus])).page(params[:page]).per(10)
+    end
+    shisetsu_name_value = params[:text][:shisetsu_name]
+    category_selected = params[:category]
+
+    chiku_all_check = true
+    Chiku.all.each do |chiku|
+      chiku_array.push([chiku.name, chiku_checkbox(params[:check][:chikus], chiku.name)])
+      chiku_all_check = false unless chiku_checkbox(params[:check][:chikus], chiku.name)
+    end
+
+    if @facilities.blank?
+      flash.now[:alert] = "検索結果が存在しませんでした。"
+    end
+
+    @gmap_hash = gmap_hash(@facilities)
+
+    search_param = Hash.new
+    search_param[:chiku_array] = chiku_array
+    search_param[:chiku_all_check] = chiku_all_check
+    search_param[:category_selected] = category_selected
+    search_param[:shisetsu_name] = shisetsu_name_value
+    @search_param = search_param
+    session["search_param"] = @search_param
+
+    render "index"
+
   end
 
   def show
 
-    index
+    # 一度セッションに入れるとシンボルが文字列に変わるため
+    @search_param = session["search_param"].symbolize_keys!
     @facility = Facility.find(params[:id])
+    @gmap_hash = gmap_hash(@facility)
+
+  end
+
+  private
+
+  def side_bar(init)
   end
 
   def chiku_checkbox(chiku_array, target_chiku)
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # root 'csvdata#insert'
   root 'facility#index'
   # Example of regular route:
-  post '/' => 'facility#index'
+  post '/' => 'facility#search'
   get '/facility/:id' => "facility#show"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
